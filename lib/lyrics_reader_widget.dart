007472eb85f5e76795ab95ede226af8856b4c8b5
M AB REHMAN <tritechuzaifa@gmail.com>
2024-02-20 10:39:32 +0500

    hello

--- lib/lyrics_reader_widget.dart
+++ lib/lyrics_reader_widget.dart
@@ -338,9 +338,19 @@ class LyricReaderState extends State<LyricsReader>
           if (widget.model.isNullOrEmpty) {
             return widget.emptyBuilder?.call() ?? Container();
           }
-          return CustomPaint(
-            painter: lyricPaint,
-            size: mSize,
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                child: Image.network('https://robohash.org/1234.png?set=set4'),
+              ),
+              CustomPaint(
+                painter: lyricPaint,
+                size: mSize,
+              ),
+            ],
           );
         },
       ),
